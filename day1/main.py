0c2dadcbe25c2a6063046dc4c4f70c4113e93433
Jakub Kobus <kubcio.kk@gmail.com>
2023-12-03 22:06:18 +0100

    day 1

---
+++ day1/main.py
@@ -0,0 +1,50 @@
+def filterDigits(line: str) -> str:
+    filtered = ''
+    for c in line:
+        if c.isdigit():
+            filtered += c
+    return filtered
+
+def repairDigits(line: str) -> str:
+    digits = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    digit = ''
+    start = 0
+    out = ''
+    for c in line:
+        if c.isdigit():
+            out += c
+            continue
+        
+        digit += c
+        for i in range(len(digits)):
+            if digits[i] in digit:
+                out += str(i + 1)
+                digit = digit[-1]
+                break
+            
+    return out
+
+# Part 1
+with open('day1/input.txt', 'r') as f:
+    lines = [l.strip() for l in f.readlines()]
+    lines = [*map(filterDigits, lines)]
+    
+    sum_ = 0
+    
+    for line in lines:
+        sum_ += int(line[0]) * 10 + int(line[-1])
+        
+    print(sum_)
+
+# Part 2
+with open('day1/input.txt', 'r') as f:
+    lines = [l.strip() for l in f.readlines()]
+    lines = [*map(repairDigits, lines)]
+    lines = [*map(filterDigits, lines)]
+    
+    sum_ = 0
+    
+    for line in lines:
+        sum_ += int(line[0]) * 10 + int(line[-1])
+        
+    print(sum_)
