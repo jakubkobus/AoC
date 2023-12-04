a9f6d494031ee63211e28bb427929ed2427da423
Jakub Kobus <kubcio.kk@gmail.com>
2023-12-04 19:41:14 +0100

    day 4 part 2

--- 2023/day4/day4-2.py
+++ 2023/day4/day4-2.py
@@ -5,14 +5,16 @@ def fetchCardInfo(line: str) -> list:
     pulled, winning = gameData.split('|')
     return [pulled.split(), winning.split()]
 
-with open('2023/day4/sample.txt', 'r') as f:
+with open('2023/day4/input.txt', 'r') as f:
     lines = [l.strip() for l in f.readlines()]
     cardsInfo = map(fetchCardInfo, lines)
     
-    sum_ = 0
+    cardInstances = [1 for _ in range(len(lines))]
     
-    for card in cardsInfo:
+    for idx, card in enumerate(cardsInfo):
         matching = np.intersect1d(card[0], card[1])
-        sum_ += (2 ** (len(matching) - 1)) if len(matching) > 0 else 0
+        for _ in range(cardInstances[idx]):
+            for i in range(idx, idx + len(matching)):
+                cardInstances[i + 1] += 1
     
-    print(sum_)
+    print(sum(cardInstances))
