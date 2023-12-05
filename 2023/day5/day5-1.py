eb2fb7a93f72fd733675e68418fc5a251c35634f
Jakub Kobus <kubcio.kk@gmail.com>
2023-12-05 18:08:45 +0100

    day 5 part 1

---
+++ 2023/day5/day5-1.py
@@ -0,0 +1,38 @@
+def mapValues(to, start, length, seed):
+    if seed >= start and seed < start + length:
+        return seed + to - start
+    return seed
+
+with open('2023/day5/input.txt', 'r') as f:
+    maps   = ['seed', 'soil', 'fertilizer', 'water', 'light', 'temperature', 'humidity', 'location']
+    values = [    [],     [],           [],      [],      [],            [],         [],         []]
+    
+    i, first = 0, True
+    for idx, line in enumerate(f.readlines()):
+        line = line.strip()
+        
+        if idx == 0:
+            line = line.split(': ')[1]
+            first = False
+            
+        if line == '':
+            i += 1
+            first = True
+            continue
+        
+        if not first:
+            values[i].append([*map(int, line.split())])
+            
+        first = False
+
+    locations = []
+    for value in values[0][0]:
+        seed = value
+        for step in range(1, 1 + len(values) - 1):
+            for map_ in values[step]:
+                temp = seed
+                seed = mapValues(*map_, temp)
+                if seed != temp: break
+        locations.append(seed)
+    
+    print(min(locations))
