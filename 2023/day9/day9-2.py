d0c8d843734facc23f0ff3329776d449ba2d8a43
Jakub Kobus <kubcio.kk@gmail.com>
2023-12-09 15:04:44 +0100

    day 9

---
+++ 2023/day9/day9-2.py
@@ -0,0 +1,26 @@
+def predict(line: str) -> int:
+    numbers = [*map(int, line.split())][::-1]
+    
+    lowerLevels, i = [numbers], 1
+    while True:
+        lowerLevels.append([])
+        last = 0
+        for idx, num in enumerate(lowerLevels[i - 1]):
+            if idx == 0:
+                last = num
+                continue
+            
+            lowerLevels[i].append(num - last)
+            last = num
+        
+        if all(x == 0 for x in lowerLevels[i]): break
+        
+        i += 1
+    
+    return sum([l[-1] for l in lowerLevels])
+
+with open('2023/day9/input.txt', 'r') as f:
+    lines = [l.strip() for l in f.readlines()]
+    
+    res = sum([predict(line) for line in lines])
+    print(res)
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # *Advent of Code* annual challenge solutions
 | Year |  ⭐  |
 | ---- | ---- |
-| 2023 |  15  |
+| 2023 |  17  |
 | 2022 |  17  |
